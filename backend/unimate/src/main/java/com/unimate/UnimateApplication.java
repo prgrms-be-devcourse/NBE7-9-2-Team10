6314f961caf88b6f0a4eb867fc28ff31d214039d
weilim0513-tech <weilim0513@gmail.com>
2025-10-14 13:52:22 +0900

    back init

---
+++ backend/unimate/src/main/java/com/unimate/UnimateApplication.java
@@ -0,0 +1,13 @@
+package com.unimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnimateApplication.class, args);
+    }
+
+}
