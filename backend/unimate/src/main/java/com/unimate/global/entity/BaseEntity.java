2152793460a00f4b05121e01d16da1350b2cf062
weilim0513-tech <weilim0513@gmail.com>
2025-10-14 15:31:15 +0900

    BaseEntity Setting

---
+++ backend/unimate/src/main/java/com/unimate/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.unimate.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
