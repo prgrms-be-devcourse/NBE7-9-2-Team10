3ada48512f8abe7df57ef1da57cf5d5f932c5346
weilim0513-tech <weilim0513@gmail.com>
2025-10-16 10:31:06 +0900

    fix(userMatchPreference): UserMatchPreferenceController 파라미터 클래스 변경

--- backend/unimate/src/main/java/com/unimate/domain/userMatchPreference/controller/UserMatchPreferenceController.java
+++ backend/unimate/src/main/java/com/unimate/domain/userMatchPreference/controller/UserMatchPreferenceController.java
@@ -4,10 +4,12 @@ package com.unimate.domain.userMatchPreference.controller;
 import com.unimate.domain.userMatchPreference.dto.MatchPreferenceRequest;
 import com.unimate.domain.userMatchPreference.dto.MatchPreferenceResponse;
 import com.unimate.domain.userMatchPreference.service.UserMatchPreferenceService;
+import com.unimate.global.jwt.CustomUserPrincipal;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,17 +19,13 @@ public class UserMatchPreferenceController {
 
     private final UserMatchPreferenceService userMatchPreferenceService;
 
-    // @PutMapping("/me/preferences")
-    @PutMapping("/{userId}/preferences")
+    @PutMapping("/me/preferences")
     public ResponseEntity<MatchPreferenceResponse> updateMyMatchPreference(
-             @AuthenticationPrincipal UserDetailsImpl userDetails,
+             @AuthenticationPrincipal CustomUserPrincipal user,
              @RequestBody MatchPreferenceRequest requestDto
-            // 이하 postman 테스트 용 파라미터
-            // @PathVariable("userId") Long userId,
-            // @Valid @RequestBody MatchPreferenceRequest requestDto
               ) {
 
-         Long userId = userDetails.getUserId();
+         Long userId = user.getUserId();
 
         MatchPreferenceResponse responseDto = userMatchPreferenceService.updateMyMatchPreferences(userId, requestDto);
         return ResponseEntity.ok(responseDto);
