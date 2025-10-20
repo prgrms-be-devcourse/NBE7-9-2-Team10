fee5be32c0ca918acfd387de64fcb7be4c403c2c
BackSeungBeom <seungbeom1234@gmail.com>
2025-10-20 17:54:49 +0900

    fix: api 변경

--- backend/unimate/src/main/java/com/unimate/domain/user/user/controller/UserController.java
+++ backend/unimate/src/main/java/com/unimate/domain/user/user/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/user")
 @Validated
 public class UserController {
     private final UserService userService;
