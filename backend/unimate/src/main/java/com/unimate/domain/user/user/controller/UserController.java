14826aedf3fc49d83fc0ccba8298ca27d5a045aa
ahnbs <ahnbs0@gmail.com>
2025-10-20 16:33:11 +0900

    refactor(FE): 최신 커밋으로 rebase후 충돌해결

--- backend/unimate/src/main/java/com/unimate/domain/user/user/controller/UserController.java
+++ backend/unimate/src/main/java/com/unimate/domain/user/user/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.unimate.domain.user.user.controller;
 
-import com.unimate.domain.user.user.dto.UserSignupRequest;
-import com.unimate.domain.user.user.dto.UserSignupResponse;
+
+import com.unimate.domain.user.user.entity.User;
 import com.unimate.domain.user.user.service.UserService;
+import com.unimate.global.jwt.CustomUserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
