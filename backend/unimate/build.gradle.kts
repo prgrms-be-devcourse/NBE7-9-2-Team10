ff565c2cc7cfc29879b6407436d61fe25305a941
BackSeungBeom <seungbeom1234@gmail.com>
2025-10-23 09:46:09 +0900

    feat: swagger를 위한 라이브러리 추가

--- backend/unimate/build.gradle.kts
+++ backend/unimate/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     implementation ("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 }
 
 tasks.withType<Test> {
