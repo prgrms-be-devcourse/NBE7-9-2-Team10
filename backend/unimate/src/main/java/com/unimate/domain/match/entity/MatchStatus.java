255b3e90196ce7f8c8139b6e9193d7c4495b6b60
HongRae-Kim <ghdfo7882@gmail.com>
2025-10-24 22:30:12 +0900

    fix(match): 좋아요 상태 관련 로직 수정

--- backend/unimate/src/main/java/com/unimate/domain/match/entity/MatchStatus.java
+++ backend/unimate/src/main/java/com/unimate/domain/match/entity/MatchStatus.java
@@ -1,6 +1,7 @@
 package com.unimate.domain.match.entity;
 
 public enum MatchStatus {
+    NONE("관계 없음"),
     PENDING("대기"),
     ACCEPTED("수락"),
     REJECTED("거절");
--- backend/unimate/src/main/java/com/unimate/domain/match/entity/MatchType.java
+++ backend/unimate/src/main/java/com/unimate/domain/match/entity/MatchType.java
@@ -1,6 +1,7 @@
 package com.unimate.domain.match.entity;
 
 public enum MatchType {
+    NONE("관계 없음"),
     LIKE("좋아요"),
     REQUEST("정식 룸메 신청");
 
